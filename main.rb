5c1176322ef68974250bbfe40a6bf19041c22904
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-10 13:42:56 -0600

    display git config

--- main.rb
+++ main.rb
@@ -7,14 +7,17 @@ class Main
   def self.menu
     puts 'Main Menu'.colorize(:cyan)
     puts '1: Enter git command'.colorize(:cyan)
-    puts '2: Exit'.colorize(:cyan)
+    puts '2: View git config'.colorize(:cyan)
+    puts '3: Exit'.colorize(:cyan)
     choice = gets.to_i
     case choice
     when 1
       puts "Enter a gets command".colorize(:green)
       Git.puts_git(gets.strip)
     when 2
-      exit
+      Git.config
+    when 3
+        exit
     else
       puts 'Invalid choice'.colorize(:red)
       menu
